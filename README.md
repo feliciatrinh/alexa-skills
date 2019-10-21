94b9b7633aa6c6648e3fe913fda3aa47b73a9d89
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2019-10-20 19:01:39 -0700

    published MAMAMOO Trivia

---
+++ README.md
@@ -0,0 +1,6 @@
+# Alexa Skills
+
+## MAMAMOO Trivia
+MAMAMOO Trivia is a trivia skill that tests your knowledge on MAMAMOO, a South Korean girl
+group with four members: Solar, Moonbyul, Wheein, and Hwasa.  
+Check it out [here](https://tinyurl.com/mamamootrivia)!
---
+++ mamamoo-trivia/lambda/questions.js
@@ -0,0 +1,541 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'Who was the voice of the moomoo god during their four season four color concerts?': [
+        'wheein',
+        'Moonbyul',
+        'solar',
+        'Hwasa',
+      ],
+    },
+    {
+      'As of 2019, what is the name of Mamamoo\'s management company?': [
+        'Rainbow bridge world',
+        'world agency',
+        'over the bridge',
+        'rainbow',
+      ],
+    },
+    {
+      'What is Mamamoo\'s official debut date?': [
+        'June 19, 2014',
+        'June 29, 2014',
+        'June 9, 2014',
+        'June 16, 2014',
+        'June 26, 2014',
+      ],
+    },
+    {
+      'Solar was discoverd by RBW agency after singing what song?': [
+        'I will always love you by Whitney Housten',
+        'Back to black by Amy Whinehouse',
+        'Love is you by Crisette Michele',
+        'Save me from myself by Christina Aguilera',
+      ],
+    },
+    {
+      'Where did Mamamoo go for their fifth aniversary trip?': [
+        'Boracay',
+        'Bali',
+        'Maldives',
+        'Japan',
+        'Jeju Island',
+      ],
+    },
+    {
+      'What of the following is not true as of year 2019?': [
+        'Hwasa can play the piano',
+        'Solar can play the guitar',
+        'Moonbyul can play the piano',
+        'Wheein can play the guitar',
+      ],
+    },
+    {
+      'Mamamoo is known to have lip synced only once since their debut. Which song did they lip sync to?': [
+        'New York',
+        'You\'re the best',
+        'Um oh ah yeah',
+        'Gentlemen',
+        'Ah oop',
+        'Memory',
+        'Yes I am',
+        'Starry Night',
+        'Egotistic',
+        'Wind flower',
+      ],
+    },
+    {
+      'Mamamoo won their first music show win with what song?': [
+        'You\'re the best',
+        'decalcomanie',
+        'um oh ah yeah',
+        'starry night',
+        'Egotistic',
+        'wind flower',
+        'Go go bay bay',
+      ],
+    },
+    {
+      'Where did they film their starry night music video?': [
+        'New Zealand',
+        'Australia',
+        'Jeju Island',
+        'Denmark',
+      ],
+    },
+    {
+      'Wheein used to go on drives with her mom a lot. What song did they always listen to?': [
+        'By your side by Sade',
+        'Stuck by stacie orrico',
+        'the love we got by Karina',
+        'cry me out by pixie lott',
+      ],
+    },
+    {
+      'Which name was a potential group name before they settled on the name Mamamoo?': [
+        'Belly button',
+        'Queen bees',
+        'Vocal Queens',
+        'Flower',
+      ],
+    },
+    {
+      'Their first music show nomination was during what era?' : [
+        'Piano Man',
+        'Pink Funky',
+        'hello',
+        'Melting',
+        'Yellow flower',
+        'Blue S',
+        'Red moon',
+        'white wind',
+      ],
+    },
+    {
+      'Which member has the smallest shoe size?': [
+        'wheein',
+        'Moonbyul',
+        'solar',
+        'Hwasa',
+      ],
+    },
+    {
+      'Solar and Moonbyul have made custom gifts for Moomoos. Which one did they not make together for Moomoos as of 2019?': [
+        'rings',
+        'cups',
+        'bracelets',
+        'cards',
+      ],
+    },
+    {
+      'Which girl group did Mamamoo appear with on Show time?': [
+        'g friend',
+        'red velvet',
+        'twice',
+        'exid',
+        'girls generation',
+      ],
+    },
+    {
+      'What do they do before they preform Peppermint chocolate at university festivals?': [
+        'Ask random couples to kiss',
+        'ask random couples to hug',
+        'ask random couples to hold hands',
+        'ask random couples to high five',
+      ],
+    },
+    {
+      'As of 2019, how many times has Mamamoo won on Immortal Songs?': [
+        'twice',
+        'three times',
+        'once',
+        'four times',
+        'never',
+      ],
+    },
+    {
+      'Moomoo\'s first fan event for Mamamoo involved holding signs saying never letting go during which song?': [
+        'love lane',
+        'mister ambiguous',
+        'I love too',
+        'star wind flower sun',
+      ],
+    },
+    {
+      'Who was the first Mamamoo member to preform in garter belts?': [
+        'Moonbyul',
+        'Wheein',
+        'Solar',
+        'Hwasa',
+      ],
+    },
+    {
+      'Which color is not one of the original member colors?': [
+        'red',
+        'orange',
+        'pink',
+        'green',
+      ],
+    },
+    {
+      'Who is the youngest member of Mamamoo?': [
+        'Hwasa',
+        'Wheein',
+        'Solar',
+        'Moonbyul',
+      ],
+    },
+    {
+      'Which member is the best cook?': [
+        'Hwasa',
+        'Wheein',
+        'Solar',
+        'Moonbyul',
+      ],
+    },
+    {
+      'Which member has a different blood type from the other three?': [
+        'Hwasa',
+        'Wheein',
+        'Solar',
+        'Moonbyul',
+      ],
+    },
+    {
+      'Which of Wheein\'s toys did Moonbyul ruin during their four seasons four colors concert?': [
+        'yellow ducky',
+        'little pony',
+        'clay figure',
+        'pokemon',
+      ],
+    },
+    {
+      'Which episode of Mamamoo TV is the famous playing in the car episode?': [
+        'Season 1 episode 6',
+        'Season 1 episode 5',
+        'Season 1 episode 7',
+        'Season 1 episode 4',
+      ],
+    },
+    {
+      'Which character did Wheein appear as on King of Masked Singer?': [
+        'Half moon',
+        'moon and stars',
+        'sunshine',
+        'comet girl',
+      ],
+    },
+    {
+      'What did Solar appear as on the show King of Masked Singer?': [
+        'Sunflower',
+        'Rose',
+        'Disney princess',
+        'swan queen',
+      ],
+    },
+    {
+      'Which character did Hwasa appear as on King of Masked Singer?': [
+        'aerobic girl',
+        'the athlete',
+        'pink warrior',
+        'jazzy girl',
+      ],
+    },
+    {
+      'What did Moonbyul appear as on the show King of Masked Singer?': [
+        'Swan',
+        'pink power ranger',
+        'bird girl',
+        'starry girl',
+      ],
+    },
+    {
+      'Which group did Mamamoo appear with on JYP\'s Party People?': [
+        'B1A4',
+        'B to B',
+        'B. A. P.',
+        'Block B',
+      ],
+    },
+    {
+      'With which one of Wheein\'s tattoos did she originally make a mistake?': [
+        'the one on her finger',
+        'the one on her arm',
+        'the one on her neck',
+        'the one near her ankle',
+      ],
+    },
+    {
+      'Hwasa used to skip kindergarten to do what?': [
+        'see the flowers',
+        'play with Wheein',
+        'sleep',
+        'sing at home',
+      ],
+    },
+    {
+      'When did Hwasa and Wheein first meet?': [
+        'in middle school',
+        'in elementary school',
+        'in high school',
+        'in choir class',
+      ],
+    },
+    {
+      'What is the name of Wheein\'s first single album?': [
+        'magnolia',
+        'easy',
+        'soar',
+        'goodbye',
+      ],
+    },
+    {
+      'What does Mamamoo\'s fandom name mean?': [
+        'radish',
+        'love',
+        'the sound cows make',
+        'together as one',
+      ],
+    },
+    {
+      'What was the first song that Mamamoo wrote for their fans?': [
+        'I love too',
+        'star wind flower sun',
+        'I\'m your fan',
+        'I miss you',
+      ],
+    },
+    {
+      'What is the name of Moonbyul\'s first web drama?': [
+        'start love',
+        'walking along the flower road',
+        'in sync',
+        'love at first sight',
+      ],
+    },
+    {
+      'Who was Solar\'s partner in We Got Married?': [
+        'Eric Nam',
+        'Paul Kim',
+        'Park Jin Young',
+        'Eddie Kim',
+      ],
+    },
+    {
+      'Which one was not one of Solar\'s characters in their Saturday Night Live skit?': [
+        'princess',
+        'squirtle',
+        'homeless person',
+        'mermaid',
+      ],
+    },
+    {
+      'While in a waiting room, Mamamoo played around with helium filled balloons gifted by which group?': [
+        'Lovelyz',
+        'Oh my girl',
+        'A. pink',
+        'G friend',
+      ],
+    },
+    {
+      'What is supposedly Mamamoo\'s average height?': [
+        '160 centimeters',
+        '155 centimeters',
+        '165 centimeters',
+        '150 centimeters',
+        '170 centimeters',
+      ],
+    },
+    {
+      'Who was the only member to not dress up as a man in their Um Oh Ah Yeah music video?': [
+        'Solar',
+        'Wheein',
+        'Moonbyul',
+        'Hwasa',
+      ],
+    },
+    {
+      'What is the name of Mamamoo\'s first full-length album?': [
+        'Melting',
+        'Yellow flower',
+        'Memory',
+        'Purple',
+        'pink funky',
+      ],
+    },
+    {
+      'Have any of the members used fake bangs?': [
+        'yes, Moonbyul during their Hello era',
+        'yes, wheein during their memory era',
+        'no, never',
+        'all of them have',
+      ],
+    },
+    {
+      'Who is the worst at drinking alcohol?': [
+        'Solar',
+        'Wheein',
+        'Hwasa',
+        'Moonbyul',
+      ],
+    },
+    {
+      'Hwasa is a fan of which Hong Kong actor?': [
+        'Leslie Cheung',
+        'Andy Lau',
+        'Jacky Cheung',
+        'Tony Leung',
+      ],
+    },
+    {
+      'As of 2019, who is the only member with no tattoos?': [
+        'Solar',
+        'Wheein',
+        'Hwasa',
+        'Moonbyul',
+      ],
+    },
+    {
+      'What is Hwasa\'s saint name?': [
+        'Maria',
+        'Theresa',
+        'Anne',
+        'Ava',
+      ],
+    },
+    {
+      'Which of these is Moonbyul\'s tattoo on her upper right arm?': [
+        'family portrait',
+        'don\'t forget to love yourself in French',
+        'upside down head',
+        'timeline',
+      ],
+    },
+    {
+      'As of 2019, who is the only member to have an Instagram account?': [
+        'Moonbyul',
+        'Wheein',
+        'Hwasa',
+        'Solar',
+      ],
+    },
+    {
+      'As of 2019, who is the only member to have an Youtube channel?': [
+        'Solar',
+        'Wheein',
+        'Hwasa',
+        'Moonbyul',
+      ],
+    },
+    {
+      'How many sisters does Hwasa have?': [
+        'two, both older',
+        'one younger sister',
+        'three older sisters',
+        'one older sister',
+      ],
+    },
+    {
+      'How many sisters does Moonbyul have?': [
+        'two, both younger',
+        'one younger sister',
+        'three younger sisters',
+        'one older sister',
+      ],
+    },
+    {
+      'The members say that Wheein resembles what animal?': [
+        'a Jindo dog',
+        'a cat',
+        'a bird',
+        'a bunny',
+      ],
+    },
+    {
+      'Which of these creatures is not associated with Solar?': [
+        'dog',
+        'dragon',
+        'bunny',
+        'squirtle',
+      ],
+    },
+    {
+      'Moonbyul is said to resemble which animal?': [
+        'a hamster',
+        'a cat',
+        'a bird',
+        'a bunny',
+      ],
+    },
+    {
+      'Which of these was one of the first Mamamoo symbols?': [
+        'the mustache',
+        'the letter M',
+        'a radish',
+        'music notes',
+      ],
+    },
+    {
+      'Which EXO member is Moonbyul said to resemble?': [
+        'Xiu Min',
+        'Lu han',
+        'Lay',
+        'Kai',
+      ],
+    },
+    {
+      'What occupation did Solar consider before becoming a singer?': [
+        'stewardess',
+        'police officer',
+        'actress',
+        'life coach',
+      ],
+    },
+    {
+      'Moonbyul grew up wanting to become which of the following?': [
+        'police officer',
+        'English teacher',
+        'actress',
+        'choreographer',
+      ],
+    },
+    {
+      'Mamamoo sang the guide for a song that was originally going to go to 4 minute. Which song was it?': [
+        'No No No',
+        'funky boy',
+        'freaking shoes',
+        'self camera',
+      ],
+    },
+    {
+      'What is Wheein\'s cat named after?': [
+        'a yogurt brand',
+        'a character in a dream',
+        'her mom\'s favorite comic book character',
+        'a candy name',
+      ],
+    },
+    {
+      'Hwasa was awarded Rookie Female of the Year in Variety for her appearances in what program?': [
+        'I live alone',
+        'hyena on the keyboard',
+        'happy together',
+        'we will channel you',
+      ],
+    },
+    {
+      'As of 2019, Moonbyul has been consistently wearing suits from which brand?': [
+        'Thom Browne',
+        'Tommy Hilfiger',
+        'Gucci',
+        'Yves Saint Laurant',
+      ],
+    },
+  ],
+};
